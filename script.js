9bfb4c059c9e5a16771e567cbf89bc6f296e61ac
Bitu Abraham <120098241+bituB2@users.noreply.github.com>
2023-03-28 17:42:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const delay = document.getElementById("delay");
 const btn = document.getElementById("btn");
 const output = document.getElementById("output");
 
-//your code here
+ async function showMessageAfterDelay(message, delay) {
+        await new Promise((resolve) => setTimeout(resolve, delay));
+        return message;
+      }
 async function showMessage() {
   const message = text.value;
   const delayVal = delay.value;
